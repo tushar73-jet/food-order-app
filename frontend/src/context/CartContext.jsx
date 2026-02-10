42e675038b34a97f87c6ff1b6193ec85fabd2e19
Tushar Kumar <184378329+tushar73-jet@users.noreply.github.com>
2026-02-10 19:35:15 +0530

    fix cart issue

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,11 +1,18 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const CartContext = createContext();
 
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const storedCart = localStorage.getItem("cart");
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (product) => {
     setCartItems((prevItems) => {
